f9c820e78569f2cd8cf13a741184de5bda09ebf2
Marc-André Leduc <marc.andre.leduc@gmail.com>
2021-09-12 07:12:27 -0400

    git remote add origin https://github.com/ZarkusProg/ProjetAngular2.git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MpcComponent } from './composants/mpc/mpc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MpcComponent
   ],
   imports: [
     BrowserModule
